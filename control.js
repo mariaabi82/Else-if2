de51fe2efcae90f0244a2d1b428a2df0ad12c4f3
mariaabi82 <maria.abi1982@gmail.com>
2019-11-01 21:41:08 -0300

    subo ejercicio 7

---
+++ control.js
@@ -0,0 +1,11 @@
+let notaAlumne = prompt(`Ingresar la nota de le alumne, de 1 a 10`)
+
+if (notaAlumne >= 9) {
+    alert(`La nota es Excelente`)
+}
+else if (notaAlumne < 9 && notaAlumne >= 6) {
+    alert(`La nota es buena`)
+}
+else {
+    alert(`El examen no está aprobado`)
+}
